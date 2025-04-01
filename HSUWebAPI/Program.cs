7d91ba0269cb8d4601e4168a4c5f0b8b8fa4ae7f
Tran Gia Nguyen Phong <phong.tgn.coder@gmail.com>
2025-04-01 11:19:47 +0700

    HSUApp-login-7 [BE]: add CORS

--- HSUWebAPI/Program.cs
+++ HSUWebAPI/Program.cs
@@ -9,6 +9,15 @@ namespace HSUWebAPI
 
             // Add services to the container
             builder.Services.AddControllers();
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+            });
 
             var app = builder.Build();
 
