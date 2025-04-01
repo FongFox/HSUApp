e2b4d3fa288560d25789535895b71cc3770ed4d4
Tran Gia Nguyen Phong <phong.tgn.coder@gmail.com>
2025-04-01 08:23:49 +0700

    HSUApp-login-3 [BE]: create menu item model

---
+++ HSUMauiApp/Models/MenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSUMauiApp.Models
+{
+    internal class MenuItem
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+        public string targetPage { get; set; }
+        public string iconColor { get; set; }
+
+    }
+}
--- HSUMauiApp/Models/User.cs
+++ HSUMauiApp/Models/User.cs
@@ -8,5 +8,7 @@ namespace HSUMauiApp.Models
 {
     internal class User
     {
+        public string Name { get; set; }
+        public string Password { get; set; }
     }
 }
